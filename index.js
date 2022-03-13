7f2a3fcc66158ddfdfc6642d4c520dc354e1af39
Jacob <mrjacobtaiwan@gmail.com>
2022-03-13 12:18:31 -0400

    Created keys file, removed key logic from index

--- index.js
+++ index.js
@@ -102,20 +102,7 @@ const foreground = new Sprite({
   image: foregroundImage
 })
 
-const keys = {
-  w: {
-    pressed: false
-  },
-  a: {
-    pressed: false
-  },
-  s: {
-    pressed: false
-  },
-  d: {
-    pressed: false
-  }
-}
+
 
 const movables = [background, ...boundaries, foreground, ...battleZones]
 
@@ -146,65 +133,6 @@ function animate() {
 
   let moving = true
   player.animate = false
-
-  if (battle.initiated) return
-
-  // activate a battle
-  if (keys.w.pressed || keys.a.pressed || keys.s.pressed || keys.d.pressed) {
-    for (let i = 0; i < battleZones.length; i++) {
-      const battleZone = battleZones[i]
-      const overlappingArea =
-        (Math.min(
-          player.position.x + player.width,
-          battleZone.position.x + battleZone.width
-        ) -
-          Math.max(player.position.x, battleZone.position.x)) *
-        (Math.min(
-          player.position.y + player.height,
-          battleZone.position.y + battleZone.height
-        ) -
-          Math.max(player.position.y, battleZone.position.y))
-      if (
-        rectangularCollision({
-          rectangle1: player,
-          rectangle2: battleZone
-        }) &&
-        overlappingArea > (player.width * player.height) / 2 &&
-        Math.random() < 0.01
-      ) {
-        // deactivate current animation loop
-        window.cancelAnimationFrame(animationId)
-
-        audio.Map.stop()
-        audio.initBattle.play()
-        audio.battle.play()
-
-        battle.initiated = true
-        gsap.to('#overlappingDiv', {
-          opacity: 1,
-          repeat: 3,
-          yoyo: true,
-          duration: 0.4,
-          onComplete() {
-            gsap.to('#overlappingDiv', {
-              opacity: 1,
-              duration: 0.4,
-              onComplete() {
-                // activate a new animation loop
-                initBattle()
-                animateBattle()
-                gsap.to('#overlappingDiv', {
-                  opacity: 0,
-                  duration: 0.4
-                })
-              }
-            })
-          }
-        })
-        break
-      }
-    }
-  }
 
   if (keys.w.pressed && lastKey === 'w') {
     player.animate = true
@@ -318,46 +246,7 @@ function animate() {
 }
 // animate()
 
-let lastKey = ''
-window.addEventListener('keydown', (e) => {
-  switch (e.key) {
-    case 'w':
-      keys.w.pressed = true
-      lastKey = 'w'
-      break
-    case 'a':
-      keys.a.pressed = true
-      lastKey = 'a'
-      break
 
-    case 's':
-      keys.s.pressed = true
-      lastKey = 's'
-      break
-
-    case 'd':
-      keys.d.pressed = true
-      lastKey = 'd'
-      break
-  }
-})
-
-window.addEventListener('keyup', (e) => {
-  switch (e.key) {
-    case 'w':
-      keys.w.pressed = false
-      break
-    case 'a':
-      keys.a.pressed = false
-      break
-    case 's':
-      keys.s.pressed = false
-      break
-    case 'd':
-      keys.d.pressed = false
-      break
-  }
-})
 
 let clicked = false
 addEventListener('click', () => {
